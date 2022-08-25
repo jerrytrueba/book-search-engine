22e5bdb83ebe2fca89b823992ee17cdbb76b4413
Jerry Trueba <jerrytrueba10@gmail.com>
2022-08-25 09:38:42 -0700

    made updates to the server js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-
 const { ApolloServer } = require('apollo-server-express');
+
 const { typeDefs, resolvers } = require('./schemas');
-const {authMiddleware} = require('./utils/auth');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -33,8 +33,8 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log('API server running on port ${PORT}!');
-    console.log('Use GraphQL at http://localhost:${PORT}${server.graphqlPath}');
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });}
 
