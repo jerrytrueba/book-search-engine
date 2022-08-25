d04ce30335637a1478d852704238ff10d0f1badb
Jerry Trueba <jerrytrueba10@gmail.com>
2022-08-25 10:12:47 -0700

    added an s on mutation

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -5,7 +5,7 @@ import { useQuery, useMutation } from "@apollo/react-hooks";
 import Auth from "../utils/auth";
 import { removeBookId, saveBookIds } from "../utils/localStorage";
 import { GET_ME } from "../utils/queries";
-import { REMOVE_BOOK } from "../utils/mutation";
+import { REMOVE_BOOK } from "../utils/mutations";
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
