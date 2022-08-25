560274fee3be1b27abd11d82a0f4431e34c2e4e7
Jerry Trueba <jerrytrueba10@gmail.com>
2022-08-24 19:13:01 -0700

    updated app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import ApolloClient from 'apollo-boost';
 import SearchBooks from './pages/SearchBooks';
@@ -26,8 +26,8 @@ function App() {
       <>
         <Navbar />
         <Routes>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
+          <Route path='/' component={SearchBooks} />
+          <Route path='/saved' component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong Page!</h1>} />
         </Routes>
       </>
